efd6750d59f6cc8d559ac49459777ae037a743ed
hasibjhb025 <hasibjhb025@student.wethinkcode.co.za>
2026-09-25 18:53:33 +0200

    feat: Add shoutoutpostapp class with jackson jms converter

---
+++ src/main/java/co/wethinkcode/shoutout/ShoutoutPostApp.java
@@ -0,0 +1,27 @@
+package co.wethinkcode.shoutout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@SpringBootApplication
+@EnableJms
+public class ShoutoutPostApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoutoutPostApp.class, args);
+    }
+
+    // Configures JMS to automatically serialize Java objects to JSON messages
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
